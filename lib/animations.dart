54c1921e41e632ce3965f683123d433378c49cc5
Eric Windmill <ericwindmill@gmail.com>
2018-04-29 10:25:19 -0700

    complete

--- lib/animations.dart
+++ lib/animations.dart
@@ -3,177 +3,81 @@ import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
 class Bar extends StatelessWidget {
+  final double marginLeft;
+  final double marginRight;
+
+  const Bar({Key key, this.marginLeft, this.marginRight}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return new Container(
-      width: 45.0,
-      height: 14.0,
+      width: 35.0,
+      height: 15.0,
+      margin: new EdgeInsets.only(left: marginLeft, right: marginRight),
       decoration: new BoxDecoration(
-        color: Theme.of(context).primaryColor,
-        borderRadius: new BorderRadius.circular(10.0),
-      ),
+          color: const Color.fromRGBO(0, 0, 255, 1.0),
+          borderRadius: new BorderRadius.circular(10.0),
+          boxShadow: [
+            new BoxShadow(
+              color: Colors.black12,
+              blurRadius: 8.0,
+              spreadRadius: 1.0,
+              offset: new Offset(1.0, 0.0),
+            ),
+            new BoxShadow(
+              color: Colors.black26,
+              blurRadius: 6.0,
+              spreadRadius: 1.5,
+              offset: new Offset(1.0, 0.0),
+            ),
+          ]),
     );
   }
 }
 
-class PivotTransition extends AnimatedWidget {
-  PivotTransition({
+class PivotBar extends AnimatedWidget {
+  final Animation<double> controller;
+  final FractionalOffset alignment;
+  final List<Animation<double>> animations;
+  final bool isClockwise;
+  final double marginLeft;
+  final double marginRight;
+
+  PivotBar({
     Key key,
     this.alignment: FractionalOffset.centerRight,
-    @required Animation<double> turns,
-    this.child,
-    this.isHalfPivot: true,
-    this.isClockwise: true,
-    @required this.name,
-  }) : super(key: key, listenable: turns);
+    @required this.controller,
+    @required this.animations,
+    @required this.isClockwise,
+    this.marginLeft = 15.0,
+    this.marginRight = 0.0,
+  }) : super(key: key, listenable: controller);
 
-  Animation<double> get turns => listenable;
-  final FractionalOffset alignment;
-  final Widget child;
-  final bool isHalfPivot;
-  final bool isClockwise;
-  final String name;
+  Matrix4 clockwiseHalf(animation) =>
+      new Matrix4.rotationZ((animation.value * math.pi * 2.0) * .5);
+  Matrix4 counterClockwiseHalf(animation) =>
+      new Matrix4.rotationZ(-(animation.value * math.pi * 2.0) * .5);
+
   @override
   Widget build(BuildContext context) {
-    final double turnsValue = turns.value;
-    var clockwiseFull = new Matrix4.rotationZ((turnsValue * math.pi * 2.0));
-    var counterClockwiseFull =
-    new Matrix4.rotationZ(-(turnsValue * math.pi * 2.0));
-    var clockwiseHalf =
-    new Matrix4.rotationZ((turnsValue * math.pi * 2.0) * .5);
-    var counterClockwiseHalf =
-    new Matrix4.rotationZ(-(turnsValue * math.pi * 2.0) * .5);
-
-    var transform;
-    switch (name) {
-      case 'barOne':
-        transform = clockwiseHalf;
-        break;
-      case 'barTwo':
-        transform = counterClockwiseHalf;
-        break;
-      case 'barThree':
-        transform = clockwiseHalf;
-        break;
-      case 'barFour':
-        transform = counterClockwiseHalf;
-        break;
+    var transformOne;
+    var transformTwo;
+    if (isClockwise) {
+      transformOne = clockwiseHalf(animations[0]);
+      transformTwo = clockwiseHalf(animations[1]);
+    } else {
+      transformOne = counterClockwiseHalf(animations[0]);
+      transformTwo = counterClockwiseHalf(animations[1]);
     }
 
     return new Transform(
-      transform: transform,
+      transform: transformOne,
       alignment: alignment,
-      child: child,
-    );
-  }
-}
-
-class StaggeredAnimation extends StatelessWidget {
-  final Animation<double> controller;
-  final Animation<double> barOneRotation;
-  final Animation<double> barTwoRotation;
-  final Animation<double> barThreeRotation;
-  final Animation<double> barFourRotation;
-
-  StaggeredAnimation({this.controller}) :
-        barOneRotation = new Tween<double>(
-          begin: 0.0,
-          end: 1.0,
-        ).animate(
-          new CurvedAnimation(
-            parent: controller,
-            curve: new Interval(
-              0.0,
-              0.25,
-              curve: Curves.linear,
-            ),
-          ),
-        ),
-        barTwoRotation = new Tween<double>(
-          begin: 0.0,
-          end: 1.0,
-        ).animate(
-          new CurvedAnimation(
-            parent: controller,
-            curve: new Interval(
-              0.25,
-              0.5,
-              curve: Curves.linear,
-            ),
-          ),
-        ),
-        barThreeRotation = new Tween<double>(
-          begin: 0.0,
-          end: 1.0,
-        ).animate(
-          new CurvedAnimation(
-            parent: controller,
-            curve: new Interval(
-              0.5,
-              0.75,
-              curve: Curves.linear,
-            ),
-          ),
-        ),
-        barFourRotation = new Tween<double>(
-          begin: 0.0,
-          end: 1.0,
-        ).animate(
-          new CurvedAnimation(
-            parent: controller,
-            curve: new Interval(
-              0.75,
-              1.0,
-              curve: Curves.linear,
-            ),
-          ),
-        );
-
-  Widget barOne = new Bar();
-  Widget barTwo = new Bar();
-  Widget barThree = new Bar();
-  Widget barFour = new Bar();
-
-  Widget buildAnimation(BuildContext context, Widget child) {
-    return Center(
-      child: new Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          new PivotTransition(
-            turns: barOneRotation,
-            alignment: FractionalOffset.centerRight,
-            name: 'barOne',
-            child: barOne,
-          ),
-          new PivotTransition(
-            turns: barTwoRotation,
-            alignment: FractionalOffset.centerRight,
-            name: 'barTwo',
-            child: barTwo,
-          ),
-          new PivotTransition(
-            turns: barThreeRotation,
-            alignment: FractionalOffset.centerRight,
-            name: 'barThree',
-            child: barThree,
-          ),
-          new PivotTransition(
-            turns: barFourRotation,
-            alignment: FractionalOffset.centerRight,
-            isHalfPivot: false,
-            name: 'barFour',
-            child: barFour,
-          ),
-        ],
+      child: new Transform(
+        transform: transformTwo,
+        alignment: alignment,
+        child: new Bar(marginLeft: marginLeft, marginRight: marginRight),
       ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new AnimatedBuilder(
-      builder: buildAnimation,
-      animation: controller,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:custom_loader/animations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class BarLoadingScreen extends StatefulWidget {
   @override
@@ -9,48 +10,160 @@ class BarLoadingScreen extends StatefulWidget {
 
 class _BarLoadingScreenState extends State<BarLoadingScreen>
     with TickerProviderStateMixin {
-  AnimationController _forwardController;
+  AnimationController _controller;
 
   @override
   initState() {
     super.initState();
-    _forwardController = new AnimationController(
-      duration: const Duration(milliseconds: 2000),
+    _controller = new AnimationController(
+      duration: const Duration(milliseconds: 3000),
       vsync: this,
     );
-    _playAnimation();
+    _controller.repeat().orCancel;
   }
-
-
-  Future<Null> _playAnimation() async {
-    try {
-      await _forwardController.forward();
-      await _forwardController.reverse();
-      _playAnimation();
-    } on TickerCanceled {
-      // the animation got canceled, probably because we were disposed
-      print(this.toString());
-    }
-  }
-
 
   @override
   dispose() {
-    _forwardController.dispose();
+    _controller?.dispose();
     super.dispose();
   }
 
+  Tween<double> tween = new Tween<double>(begin: 0.0, end: 1.00);
+  Animation<double> get stepOne => tween.animate(
+        new CurvedAnimation(
+          parent: _controller,
+          curve: new Interval(
+            0.0,
+            0.125,
+            curve: Curves.linear,
+          ),
+        ),
+      );
+  Animation<double> get stepTwo => tween.animate(
+        new CurvedAnimation(
+          parent: _controller,
+          curve: new Interval(
+            0.125,
+            0.26,
+            curve: Curves.linear,
+          ),
+        ),
+      );
+  Animation<double> get stepThree => tween.animate(
+        new CurvedAnimation(
+          parent: _controller,
+          curve: new Interval(
+            0.25,
+            0.375,
+            curve: Curves.linear,
+          ),
+        ),
+      );
+  Animation<double> get stepFour => tween.animate(
+        new CurvedAnimation(
+          parent: _controller,
+          curve: new Interval(
+            0.375,
+            0.5,
+            curve: Curves.linear,
+          ),
+        ),
+      );
+  Animation<double> get stepFive => tween.animate(
+        new CurvedAnimation(
+          parent: _controller,
+          curve: new Interval(
+            0.5,
+            0.625,
+            curve: Curves.linear,
+          ),
+        ),
+      );
+  Animation<double> get stepSix => tween.animate(
+        new CurvedAnimation(
+          parent: _controller,
+          curve: new Interval(
+            0.625,
+            0.75,
+            curve: Curves.linear,
+          ),
+        ),
+      );
+  Animation<double> get stepSeven => tween.animate(
+        new CurvedAnimation(
+          parent: _controller,
+          curve: new Interval(
+            0.75,
+            0.875,
+            curve: Curves.linear,
+          ),
+        ),
+      );
+  Animation<double> get stepEight => tween.animate(
+        new CurvedAnimation(
+          parent: _controller,
+          curve: new Interval(
+            0.875,
+            1.0,
+            curve: Curves.linear,
+          ),
+        ),
+      );
+
   Widget get forwardStaggeredAnimation {
-    return new StaggeredAnimation(
-      controller: _forwardController,
+    return new Center(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new PivotBar(
+            alignment: FractionalOffset.centerLeft,
+            controller: _controller,
+            animations: [
+              stepOne,
+              stepTwo,
+            ],
+            marginRight: 0.0,
+            marginLeft: 0.0,
+            isClockwise: true,
+          ),
+          new PivotBar(
+            controller: _controller,
+            animations: [
+              stepThree,
+              stepEight,
+            ],
+            marginRight: 0.0,
+            marginLeft: 0.0,
+            isClockwise: false,
+          ),
+          new PivotBar(
+            controller: _controller,
+            animations: [
+              stepFour,
+              stepSeven,
+            ],
+            marginRight: 0.0,
+            marginLeft: 32.0,
+            isClockwise: true,
+          ),
+          new PivotBar(
+            controller: _controller,
+            animations: [
+              stepFive,
+              stepSix,
+            ],
+            marginRight: 0.0,
+            marginLeft: 32.0,
+            isClockwise: false,
+          ),
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      child: forwardStaggeredAnimation
-    );
+    return new Container(child: forwardStaggeredAnimation);
   }
 }
 
